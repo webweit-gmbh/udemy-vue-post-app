09ac9572ec61d78d0bf3f9ebc32a6ae95ae3fcb3
Robert Rypuła <robert.rypula@gmail.com>
2022-07-09 19:17:16 +0200

    Adds types and selection of the clicked element

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import { ref } from "vue";
 
+type Period = "Today" | "This Week" | "This Month";
+
+const periods: Period[] = ["Today", "This Week", "This Month"];
+const selectedPreriod = ref<Period>("Today");
+
+const selectPeriod = (period: Period) => {
+  selectedPreriod.value = period;
+}
 </script>
 
-<template>  
+<template>
+  <pre>{{ periods }}</pre>
+  {{ selectedPreriod }}
   <nav class="is-primary panel">
     <span class="panel-tabs">
-        <a>Today</a>
-        <a>This week</a>
-        <a>This month</a>
+      <a 
+        v-for="period of periods"
+        :key="period"
+        :class="{ 'is-active': period === selectedPreriod }"
+        @click="selectPeriod(period)"
+      >
+        {{ period }}
+      </a>
     </span>
   </nav>
 </template>
