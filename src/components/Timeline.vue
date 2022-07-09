96d0b9c783f4c05e4a452e0f593bf58dc2d327d9
Robert Rypuła <robert.rypula@gmail.com>
2022-07-09 19:54:10 +0200

    Working filtering

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { DateTime } from "luxon";
+import { ref, computed } from "vue";
+
+import { Post, today, thisWeek, thisMonth } from '../posts';
 
 const periods = ["Today", "This Week", "This Month"] as const;
 type Period = typeof periods[number];
@@ -9,6 +12,25 @@ const selectedPreriod = ref<Period>("Today");
 const selectPeriod = (period: Period) => {
   selectedPreriod.value = period;
 }
+
+const posts = computed(() => {
+  return [today, thisWeek, thisMonth]
+    .map((post) => ({
+      ...post,
+      created: DateTime.fromISO(post.created)
+    }))
+    .filter((post) => {
+      if (selectedPreriod.value === "Today") {
+        return post.created >= DateTime.now().minus({ day: 1 });
+      }
+
+      if (selectedPreriod.value === "This Week") {
+        return post.created >= DateTime.now().minus({ week: 1 });
+      }
+
+      return true;
+    });
+});
 </script>
 
 <template>
@@ -25,5 +47,17 @@ const selectPeriod = (period: Period) => {
         {{ period }}
       </a>
     </span>
+
+    <div
+      v-for="post of posts"
+      :key="post.id"
+      class="panel-block"
+    >
+      <a href="#">{{ post.title }}</a>
+      &nbsp;
+      <span>
+        {{ post.created.toFormat('y MMMM d, TT') }}
+      </span>
+    </div>
   </nav>
 </template>
